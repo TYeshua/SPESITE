2b242638b8faadcdc9dbd572f7d0c6235e710928
TYeshua <thiagoyeshua01@gmail.com>
2025-06-20 12:19:11 -0300

    Commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// Substitua pelo NOME DO SEU REPOSITÓRIO
+// Substitua pelo NOME DO SEU REPOSITÓRIOaaaa
 export default defineConfig({
   base: '/SPESITE/',
   plugins: [react()],
