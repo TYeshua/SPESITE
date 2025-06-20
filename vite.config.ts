cd3f115ac9407a40373f13b0825d38bb265b10e5
TYeshua <thiagoyeshua01@gmail.com>
2025-06-20 13:01:53 -0300

    package.jon

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
+// https://vitejs.dev/config/aaaa
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
