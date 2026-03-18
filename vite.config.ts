904f73787251fb0e44afc60ad34380a566c43c25
jacare <jfssdias@hotmail.com>
2026-03-18 00:33:56 -0300

    atualizações pre deploy

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,10 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/aaaa
 export default defineConfig({
+  build: { chunkSizeWarningLimit: 1000 },
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-  base:"/SPESITE/"
+  base: "/SPESITE/"
 });
